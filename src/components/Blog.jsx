6f1c5404a1d56b2621e78a84b11f75352f2651ab
m0hossam <mohossam1326@gmail.com>
2025-11-05 19:42:12 +0200

    Finish 5.8

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-    <div>
-        {blog.title} - {blog.author}
-    </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+    const [isDetailed, setIsDetailed] = useState(false)
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+
+    return (
+        <div style={blogStyle}>
+            <h3>{blog.title} <button onClick={() => setIsDetailed(p => !p)}>{isDetailed ? 'Hide details' : 'Show details'}</button></h3>
+            {
+                isDetailed && (
+                    <div>
+                        <p>Author: {blog.author}</p>
+                        <p>Likes: {blog.likes} <button>Like</button></p>
+                        <p>URL: {blog.url}</p>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
 
 export default Blog
